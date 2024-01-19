b38dc8cb2e9d2ec650499b9cc881693a86443848
Aleksandr Prilipko <zverozabr@yandex.ru>
2024-01-19 16:35:31 +0700

    changed build sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 docker build -f ./microservices/ms-pipeline-manager/Dockerfile -t spex.pipeline.manager:latest .
+docker tag spex.pipeline.manager:latest ghcr.io/genentech/spex.pipeline.manager:latest
+docker push ghcr.io/genentech/spex.pipeline.manager:latest
